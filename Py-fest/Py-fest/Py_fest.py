8a6abd2868b7f1ab5845e7d494a4ea923d0055cb
Marshall Friedman <marshallmaf32310@gmail.com>
2026-01-09 14:06:48 -0700

    bands

--- Py-fest/Py-fest/Py_fest.py
+++ Py-fest/Py-fest/Py_fest.py
@@ -1,1 +1,28 @@
+# The initial lineup
+lineup = [
+    ("Code Play", "Indie", 30),
+    ("The Pythonistas", "Rock", 45),
+    ("Syntax Error", "Metal", 60)
+]
 
+# 1. Add the headliner
+headliner = ("The Byte Beats", "Synthwave", 90)
+lineup.append(headliner)
+
+#2
+rb = lineup.pop(0)
+print(lineup)
+lineup.append(rb)
+#3
+lb = lineup.pop(0)
+lineup.append(lb)
+# 4. Calculate Total Time
+total_minutes = 0
+for name, genre, duration in lineup:
+    total_minutes += duration
+
+print("Final Lineup:")
+for name, genre, duration in lineup:
+    print(f"{name} ({genre}) - {duration} min")
+
+print(f"\nTotal Festival Duration:
